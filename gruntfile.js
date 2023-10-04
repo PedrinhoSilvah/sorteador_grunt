1bcab9d261c46c93df778a993d65f93f35e8d53f
Pedro Miguel <pedrinhosillvah59@gmail.com>
2023-10-04 15:57:23 -0300

    commit 2

--- gruntfile.js
+++ gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt){
                 patterns: [
                     {
                         match: 'ENDERECO_DO_CSS',
-                        replacement: './styles/main.css'
+                        replacement: '../styles/main.css'
                     },
                     {
                         match: 'ENDERECO_DO_JS',
